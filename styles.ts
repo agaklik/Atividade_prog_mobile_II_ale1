70d83697e8a60f0ac309787aa8083fef466bcfcc
agaklik <112034103+agaklik@users.noreply.github.com>
2023-03-27 23:14:43 -0300

    Commit inicial

---
+++ styles.ts
@@ -0,0 +1,60 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+
+  scrollView: {
+    backgroundColor: '#FFF',
+    paddingHorizontal:15,
+    paddingTop:40
+  },
+  container:{
+    alignItems: 'center',
+  },
+  logo:{
+    width:150,
+    height:150
+  },
+  h1:{
+    color:'#000',
+    fontSize:27,
+    fontWeight:'bold',
+    marginVertical:10
+
+  },
+  h2:{
+    color:'#999',
+    fontSize:15
+  },
+  inputArea:{
+    width:'100%',
+    paddingTop:20,
+    fontWeight:'bold',
+
+
+  },
+  inputLabel:{
+    color:'#777',
+    fontSize:14,
+    marginBottom:7
+
+  },
+  inputField:{
+    borderWidth:2,
+    borderRadius:5,
+    borderColor:'#DDD',
+    fontSize:15,
+    padding:10
+
+  },
+  aditionals:{},
+  forgotBtnArea:{},
+  forgotBtnText:{},
+  button:{},
+  buttonText:{},
+  signUpArea:{},
+  signUpText:{},
+  signUpBtnText:{},
+  footerArea:{},
+  footerText:{}
+
+});
